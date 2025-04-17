614376a52f6fcca013dfce15e4d0376f062c4f1b
keith <keithhans@gmail.com>
2025-04-17 12:50:20 +0800

    add more actions to robot

--- src/iot/things/robot.py
+++ src/iot/things/robot.py
@@ -19,6 +19,54 @@ class Robot(Thing):
         self.add_method("Backward", "后退", [],
                         lambda params: self._backward())
 
+        self.add_method("Stand", "立正", [],
+                        lambda params: self._stand())
+
+        self.add_method("TurnLeft", "左转", [],
+                        lambda params: self._turn_left())
+
+        self.add_method("TurnRight", "右转", [],
+                        lambda params: self._turn_right())
+
+        self.add_method("Bow", "鞠躬", [],
+                        lambda params: self._bow())
+        
+        self.add_method("Wave", "挥手", [],
+                        lambda params: self._wave())
+        
+        self.add_method("Hug", "拥抱", [],
+                        lambda params: self._hug())
+
+    def _bow(self):
+        AGC.runActionGroup('bow')
+        print(f"[小幻机器人] 已鞠躬")
+        return {"status": "success", "message": "鞠躬"}
+    
+    def _wave(self):
+        AGC.runActionGroup('wave')
+        print(f"[小幻机器人] 已挥手")
+        return {"status": "success", "message": "挥手"}
+    
+    def _hug(self):
+        AGC.runActionGroup('hug')
+        print(f"[小幻机器人] 已拥抱")
+        return {"status": "success", "message": "拥抱"}
+    
+    def _stand(self):
+        AGC.runActionGroup('stand')
+        print(f"[小幻机器人] 已立正")
+        return {"status": "success", "message": "立正"}
+
+    def _turn_left(self):
+        AGC.runActionGroup('turn_left')
+        print(f"[小幻机器人] 已左转")
+        return {"status": "success", "message": "左转"}
+
+    def _turn_right(self):
+        AGC.runActionGroup('stand')
+        print(f"[小幻机器人] 已右转")
+        return {"status": "success", "message": "右转"}
+
     def _forward(self):
         AGC.runActionGroup('go_forward')
         print(f"[小幻机器人] 已前进")
